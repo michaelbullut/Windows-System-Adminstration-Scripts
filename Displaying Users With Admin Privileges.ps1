f89d8d0491a5e6e2c6c6287573a1dfc524e1bd2e
Michael Bullut <michael.bullut@solstice.co.ke>
2025-08-27 13:33:06 +0300

    Add files via upload

---
+++ Displaying Users With Admin Privileges.ps1
@@ -0,0 +1,8 @@
+﻿# Get a list of local groups that have administrators 
+$adminGroup = Get-LocalGroupMember -Group "Administrators" 
+
+# Output the members of the administrators group 
+Write-Host "Users with administrator privileges:" 
+foreach ($user in $adminGroup) { 
+    Write-Host $user.Name 
+} 
