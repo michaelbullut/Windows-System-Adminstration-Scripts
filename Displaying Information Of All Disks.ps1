678f7b2d9da7b8e3a8d20806d7d0e6977b1c03bc
Michael Bullut <michael.bullut@solstice.co.ke>
2025-08-27 13:00:43 +0300

    Add files via upload

---
+++ Displaying Information Of All Disks.ps1
@@ -0,0 +1,20 @@
+﻿# Get information about all volumes on the computer 
+$volumeInfo = Get-Volume 
+
+# Output volume information line by line 
+$volumeInfo | ForEach-Object { 
+    $sizeGB = [math]::Round(($_.Size / 1GB), 2) 
+    $sizeRemainingGB = [math]::Round(($_.SizeRemaining / 1GB), 2) 
+    $sizeUsedGB = $sizeGB - $sizeRemainingGB 
+
+    Write-Output "DriveLetter: $($_.DriveLetter)" 
+    Write-Output "FriendlyName: $($_.FriendlyName)" 
+    Write-Output "FileSystemType: $($_.FileSystemType)" 
+    Write-Output "DriveType: $($_.DriveType)" 
+    Write-Output "HealthStatus: $($_.HealthStatus)" 
+    Write-Output "OperationalStatus: $($_.OperationalStatus)" 
+    Write-Output "Size: $sizeGB GB" 
+    Write-Output "SizeUsed: $sizeUsedGB GB" 
+    Write-Output "SizeRemaining: $sizeRemainingGB GB" 
+    Write-Output "" 
+} 
