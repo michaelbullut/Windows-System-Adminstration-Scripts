58b287d236f82559aa028a724ec32b3b77d761c1
Michael Bullut <michael.bullut@solstice.co.ke>
2025-08-27 12:15:58 +0300

    Delete Computer Uptime.ps1

--- Computer Uptime.ps1
+++
@@ -1,3 +0,0 @@
-$os = Get-WmiObject -Class Win32_OperatingSystem
-$uptime = (Get-Date) - $os.ConvertToDateTime($os.LastBootUpTime)
-Write-Host "Client / Server has been running for: $($uptime.Days) days $($uptime.Hours) hours $($uptime.Minutes) minutes $($uptime.Seconds) seconds"
